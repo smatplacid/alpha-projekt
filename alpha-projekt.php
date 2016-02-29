e0ec29b5da7c6e2fa0dbaa8f04fceb9e1a0b1448
smatplacid <smatplacid@gmail.com>
2016-02-29 07:06:48 +0100

    version

--- alpha-projekt.php
+++ alpha-projekt.php
@@ -4,7 +4,7 @@
 Plugin Name: alpha projekt Workaround
 Plugin URI: http://alpha-projekt.de
 Description: Standardfunktionen für Theme- &amp; WordPress-Entwicklung
-Version: 12.2015
+Version: 15.2.28
 Author: Roman Hanzlik // alpha projekt GbR
 Author URI: http://www.alpha-projekt.de/kontakt/
 License: Attribution-NonCommercial 4.0 International (https://goo.gl/L9oQ68)
